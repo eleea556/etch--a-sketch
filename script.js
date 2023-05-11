fd2f30c9cf4a3d1536372e4b9713a4965428baa8
Elia <122890077+eleea556@users.noreply.github.com>
2023-05-11 15:53:04 +0200

    Add basic 16x16 grid

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 'use strict';
 
+function createSquareGrid(grid, sideSize) {
+    for (let i = 0; i < sideSize; i++) {
+        for (let j = 0; j < sideSize; j++) {
+            const cell = document.createElement('div');
+            cell.classList += "cell";
+            grid.appendChild(cell);
+        }
+    }
+}
+
+const grid = document.querySelector(".grid");
+
+let sideSize = 16;
+
+createSquareGrid(grid, sideSize);
+
