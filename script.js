d6420e8dc4720b7896d55ade8ab1e238ba2e91e7
Elia <122890077+eleea556@users.noreply.github.com>
2023-05-30 17:06:40 +0200

    Add resizing button

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 'use strict';
 
-function createSquareGrid(grid, sideSize) {
+function clearGrid(grid) {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+}
+
+function createSquareGrid(grid, sideSize, gridSize = 508) {
+    root.style.setProperty('--cell-size', gridSize/sideSize + "px");
     for (let i = 0; i < sideSize; i++) {
         for (let j = 0; j < sideSize; j++) {
             const cell = document.createElement('div');
@@ -8,18 +15,30 @@ function createSquareGrid(grid, sideSize) {
             grid.appendChild(cell);
         }
     }
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseenter', (e) => {
+            cell.style.backgroundColor = "black";
+        });
+    });
 }
 
 const grid = document.querySelector(".grid");
+const root = document.documentElement;
 
 let sideSize = 16;
 
 createSquareGrid(grid, sideSize);
 
-const cells = document.querySelectorAll(".cell");
-
-cells.forEach((cell) => {
-    cell.addEventListener('mouseenter', (e) => {
-        cell.style.backgroundColor = "black";
-    });
+const cellsNumberButton = document.querySelector("button");
+cellsNumberButton.addEventListener('click', (e) => {
+    sideSize = +prompt("Please enter the number of squares per side (max size is 75):");
+    if (sideSize === NaN || !Number.isInteger(sideSize) || sideSize <= 0) {
+        sideSize = 16;
+    }
+    if (sideSize > 75) {
+        sideSize = 75;
+    }
+    clearGrid(grid);
+    createSquareGrid(grid, sideSize);
 });
